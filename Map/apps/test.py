f1ac638d12fd25ac2f00102f3b29f7016bf90d22
Albert-Econ <87816332+Albert-Econ@users.noreply.github.com>
2023-04-29 09:43:44 +0200

    changes to test.py

--- Map/apps/test.py
+++ Map/apps/test.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import geopandas as gpd
 
 
 def app():
 
     m = leafmap.Map(center=[0, 0], zoom=2)
 
-    in_shp = '../data/countries.shp'
-    m.add_shp(in_shp, layer_name="Countries")
-
+    frankfurt = gpd.read_file("https://github.com/Albert-Econ/vier_gewinnt/blob/main/Map/1%20Land%20Prices/Land_Prices_Neighborhood_Frankfurt_am_Main.gpkg")
+    m.add_gdf(frankfurt, layer_name="Frankfurt")
+    
     m.to_streamlit(height=700)
