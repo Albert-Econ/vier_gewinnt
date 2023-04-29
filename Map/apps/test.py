60328e4da8ea958ee7a4272e2c19a315cabadc50
Albert-Econ <87816332+Albert-Econ@users.noreply.github.com>
2023-04-29 10:23:32 +0200

    changes

--- Map/apps/test.py
+++ Map/apps/test.py
@@ -7,8 +7,8 @@ def app():
 
     m = leafmap.Map(center=[0, 0], zoom=2)
 
-    frankfurt = gpd.read_file("https://github.com/Albert-Econ/vier_gewinnt/blob/main/Map/1%20Land%20Prices/Land_Prices_Neighborhood_Frankfurt_am_Main.gpkg")
-    frankfurt_geo = frankfurt["geometry"]
+    frankfurt = gpd.read_file("https://github.com/Albert-Econ/vier_gewinnt/blob/main/map/Land_Prices/frankfurt.shp")
+    #frankfurt_geo = frankfurt["geometry"]
     m.add_gdf(frankfurt_geo, layer_name="Frankfurt")
     
     m.to_streamlit(height=700)
