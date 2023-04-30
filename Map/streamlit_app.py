37ae1635a0bcb2a293aaf14a8bc998726de175e2
Albert-Econ <87816332+Albert-Econ@users.noreply.github.com>
2023-04-30 09:16:56 +0200

    changes streamlit

--- Map/streamlit_app.py
+++ Map/streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from apps import home, heatmap, upload, test  # import your app modules here
+from apps import frankfurt  # import your app modules here
 
 st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
 
@@ -8,10 +8,7 @@ st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
 # More icons can be found here: https://icons.getbootstrap.com
 
 apps = [
-    {"func": home.app, "title": "Home", "icon": "house"},
-    {"func": heatmap.app, "title": "Heatmap", "icon": "map"},
-    {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
-    {"func": test.app, "title": "Test", "icon": "bi bi-binoculars"},
+    {"func": frankfurt.app, "title": "Frankfurt", "icon": "bi bi-building"},
 ]
 
 titles = [app["title"] for app in apps]
@@ -27,23 +24,11 @@ else:
 
 with st.sidebar:
     selected = option_menu(
-        "Main Menu",
+        "Cities",
         options=titles,
         icons=icons,
         menu_icon="cast",
         default_index=default_index,
-    )
-
-    st.sidebar.title("About")
-    st.sidebar.info(
-        """
-        This web [app](https://share.streamlit.io/giswqs/streamlit-template) is maintained by [Qiusheng Wu](https://wetlands.io). You can follow me on social media:
-            [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu).
-        
-        Source code: <https://github.com/giswqs/streamlit-template>
-
-        More menu icons: <https://icons.getbootstrap.com>
-    """
     )
 
 for app in apps:
